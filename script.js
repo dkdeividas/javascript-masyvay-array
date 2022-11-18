cf8bb6ce53bf2bb1f04f6b7c26325932bd37f20d
Moksleivis <Moksleivis@kitm.local>
2022-11-18 14:02:47 +0200

    first commit

--- script.js
+++ script.js
@@ -88,12 +88,157 @@ let mokiniai = [
     vardas: "Jonas",
     grupe: "JS20",
     amzius: "30",
-     vidurkis: "9.5"
+    vidurkis: "9.5"
     },
 ]
 console.log(mokiniai[1].vardas);
 
 //loopinimo metodai, iteravimas
-mokiniai.forEach((item, index)=>{
+mokiniai.forEach((item)=>{
     console.log(item.vardas);
 });
+
+//index of metodas
+// let prekes = ["kamuolys", "lekstes", "telefonas", "kolonele", "lekstes", "telefonas", "saltiena", "paukstiena", "saldiklis", "kamuolys","telefonas"];
+
+// console.log(prekes)
+
+// let unikalus = [];
+
+// prekes.map(item=>{
+//   if(unikalus.indexOf(item) === -1){
+//     unikalus.push(item)
+//   }
+// });
+
+// console.log(unikalus)
+
+//include metodas
+let prekes = ["kamuolys", "lekstes", "telefonas", "kolonele", "lekstes", "telefonas", "saltiena", "paukstiena", "saldiklis", "kamuolys","telefonas"];
+
+console.log(prekes)
+
+let unikalus = [];
+
+prekes.map(item=>{
+  if (!unikalus.includes(item))
+    unikalus.push(item);
+});
+
+console.log(unikalus)
+
+
+// Ciklai
+// let automobiliai=["VW","Audi", "Jeep", "MINI", "Subaru", "Mazda", "Fiat", "Seat", "Honda", "Nissan"];
+// // for(let item of automobiliai){
+// //     console.log(item);
+// // }
+
+// for(let i=0; i<automobiliai.length; i++){
+//     console.log(` paprastas for loop'as ${automobiliai[i]}`);
+// }
+
+// let klasiokas = {
+//     vardas: "Deividas",
+//     klase: "JS22/2",
+//     vidurkis: 2.5,
+//     pomegiai: {
+//         1:"vaikscioti",
+//         2:"programavimas",
+//         3:"tingejimas"}
+// }
+// for(let savybe in klasiokas){
+//     console.log(savybe)
+//     console.log(klasiokas[savybe])
+//     console.log(typeof klasiokas[savybe]);
+
+
+// if(typeof klasiokas[savybe] !== "object"){
+//     console.log(savybe + " : " +klasiokas[savybe]);
+// } else{
+//     console.log(savybe + " yra -> vardinu: ")
+//     for(let antras in klasiokas[savybe]){
+//         console.log(antras+ " yra " + klasiokas[savybe][antras]);
+//     }
+// }
+// }
+
+
+let kursai = [
+    {
+        pavadinimas: "JS",
+        aprasymas: "labai lengva programavimo kalba",
+        lektorius: "Jolita",
+        kaina: 200
+    },
+    {
+        pavadinimas: "Java",
+        aprasymas: "nelabai lengva programavimo kalba",
+        lektorius: "Jonas",
+        kaina: 350
+    },
+    {
+        pavadinimas: "Python",
+        aprasymas: "labai sunki programavimo kalba",
+        lektorius: "Jon",
+        kaina: 400
+    },
+]
+// for/of skirtas masyvams
+for(let i of kursai){
+    // console.log(i);
+    for(let j in i){
+        // console.log(j);
+        //console.log(`savybe: ${j} reiksme -> ${i[j]}`);
+    }
+}
+// --------------------------------
+let kurseliai = [
+    {
+        pavadinimas: "JS",
+        aprasymas: "labai lengva programavimo kalba",
+        lektorius: "Jolita",
+        kaina: {
+            akcija: 100,
+            pagrindine: 200,
+            galutine: 50
+        },
+        tag: ["js", "programing", "lengva"]
+    },
+    {
+        pavadinimas: "Java",
+        aprasymas: "nelabai lengva programavimo kalba",
+        lektorius: "Jonas",
+        kaina: {
+            akcija: 150,
+            pagrindine: 350,
+            galutine: 70
+        },
+        tag: ["jv", "programingg", "nelabai"]
+    },
+    {
+        pavadinimas: "Python",
+        aprasymas: "labai sunki programavimo kalba",
+        lektorius: "Jon",
+        kaina: {
+            akcija: 220,
+            pagrindine: 400,
+            galutine: 120
+        },
+        tag: ["py", "programinggg", "sunku"]
+    },
+]
+for(let i=0; i<kurseliai.length; i++){
+    //console.log(kurseliai[i]);
+    for(let item in kurseliai[i]){
+        // console.log(typeof kurseliai[i][item]);
+        if(typeof kurseliai [i] [item] == 'object'){
+            //sukam dar vien cikla
+
+        }else{
+            // reiskia kad ne object, tai tiesiog consolinam
+            console.log(`${item} : ${kurseliai[i][item]}`)
+        }
+    }
+    console.log("----------------");
+}
